048d4dd1e5b59e5acd8c870626674173dfcb420a
FireLord1 <firelord.xda@lineageos.org>
2017-11-08 10:42:31 +0530

    passion: Enable DT2W
    Change-Id: If362db19982dffdf326dec36319e8f86c9cb341b
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,7 +117,7 @@ TARGET_SYSTEM_PROP += device/lenovo/passion/system.prop
 # Qualcomm support
 BOARD_USES_QC_TIME_SERVICES := true
 TARGET_POWERHAL_VARIANT := qcom
-#TARGET_TAP_TO_WAKE_NODE := "/sys/class/tp_gesture/tp_device/tp_gesture_id"
+TARGET_TAP_TO_WAKE_NODE := "/sys/board_properties/tpd_suspend_status"
 BOARD_USES_QCOM_HARDWARE := true
 
 # Recovery
