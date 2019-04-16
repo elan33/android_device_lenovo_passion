6a24e2f760da356d9841307b341945ecaa76658e
elan33 <67664518@qq.com>
2019-04-16 12:32:23 +0800

    Build health HIDL

--- device.mk
+++ device.mk
@@ -165,8 +165,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # Manifest
 PRODUCT_COPY_FILES += \
